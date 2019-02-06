1dd04e9d6094a60a71d22455e90b9861b8fc6952
RickChuck <you@example.com>
2019-02-05 17:33:31 -0700

    search working

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,19 +1,28 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-// require('dotenv').config();
-
-// const URL = "https://api.themoviedb.org/3/movie/550?api_key=";
-// const API_KEY = process.env.API_KEY;
+import $ from 'jquery';
+import MovieRow from './MovieRow'
+require('dotenv').config();
 
 class Dashboard extends Component {
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
         this.state = {
             posts: [],
             blogInput: ''
         }
-    }
 
+        // const movieRows = []
+        // movies.forEach((movie) => {
+        //     const movieRow = <MovieRow movie={movie} />
+        //     movieRows.push(movieRow)
+        // })
+
+        // this.state = {rows: movieRows}
+
+        this.performSearch() 
+    }
+    
     componentDidMount = async () => {
         let res = await axios.get('/api/getPost')
         // console.log('res.data', res.data);
@@ -21,11 +30,42 @@ class Dashboard extends Component {
             posts: res.data
         })
     }
+    
+    performSearch = (searchTerm) => {
+        console.log('perform search')
+        let API_KEY = process.env.REACT_APP_MOVIE_API_KEY;
+        const urlString = `https://api.themoviedb.org/3/search/movie?api_key=`+API_KEY+"&query="+searchTerm
+        $.ajax({
+            url: urlString,
+            success: (searchResults) => {
+                console.log("Fetched data successfully")
+                console.log(searchResults);
+                const results = searchResults.results
+                // console.log(results[0]);
 
-    // getPosters = async () => {
-    //     let res = await axios.get(URL+`${API_KEY}`)
-    //     this.setState({ movie: res.data})
-    // }
+                let movieRows = []
+
+                results.forEach((movie) => {
+                    movie.poster_src = "https://image.tmdb.org/t/p/w185"+movie.poster_path
+                    // console.log(movie.poster_path);
+                    const movieRow = <MovieRow key={movie.id} movie={movie}/>
+                    movieRows.push(movieRow)
+                })
+
+                this.setState({rows: movieRows})
+            },
+            error: (xhr, status, err) => {
+                console.error("Failed to fetch data");
+            }
+        })
+    }
+    
+    searchChangeHandler = (event) => {
+        const boundObject = this
+        const searchTerm = event.target.value
+        this.performSearch(searchTerm)
+        boundObject.performSearch(searchTerm)
+    }
 
     addPost = async () => {
         let {blogInput} = this.state
@@ -69,11 +109,39 @@ class Dashboard extends Component {
             <div className='Dashboard'>
                 <h1>You made it to the DASHBOARD</h1>
                 <div>
-                    <button onClick={this.logout}>Logout</button>
+                    <nav style={{
+                            backgroundColor: "black",
+                            display: "block",
+                            padding: 20    
+                        }}>
+
+                        <button onClick={this.logout}>Logout</button>
+                    </nav>
                 </div>
                 <div>
-                    <input onChange={(e) => this.setState({blogInput: e.target.value})} type="text"/>
-                    <button onClick={() => this.addPost()}>Submit</button>
+                    <div>
+                        <h3>Search movie posters by title.</h3>
+                        <input onChange={this.searchChangeHandler.bind(this)} style={{
+                            fontSize: 24,
+                            margin: 5,
+                            width: "75%"
+                        }} type="text"/>
+                        {this.state.rows}
+                    </div>
+                    <h4>Share your thoughts.</h4>
+                    <div>
+                        
+                    </div>
+                    <input style={{
+                            fontSize: 24,
+                            margin: 5,
+                            width: "75%",
+                            height: 50
+                        }}  onChange={(e) => this.setState({blogInput: e.target.value})} type="text"/>
+                    <button style={{
+                            fontSize: 18,
+                            margin: 5
+                        }}  onClick={() => this.addPost()}>Submit</button>
                 </div>
                 <hr/>
                 <div>{blogDisplay}</div>
